6bc98d67f158bb6958cc91f9b62ca2f64cca9767
Saalico <jblakenutt@gmail.com>
2024-11-02 16:21:54 -0400

    git_ignore shenanigans

--- style.nix
+++ style.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 let
   theme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
-  wallpaper = ./wallpaper.jpeg;
+  wallpaper = ./img.jpeg;
 in {
   stylix = {
     autoEnable = true;
